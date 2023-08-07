802f9e5c98f4d18237417422b580c936bfc90d63
Tor2H <tor.holm@live.dk>
2023-08-07 17:11:17 +0200

    setup

--- webApp/angularapp/src/app/app.module.ts
+++ webApp/angularapp/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageComponent } from './page/page.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
