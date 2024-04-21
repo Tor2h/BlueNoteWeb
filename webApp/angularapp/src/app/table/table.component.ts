ec3f0e22cde6d5ec1d4e85746c61500a1c610d74
Tor2H <tor.holm@live.dk>
2024-04-21 20:54:05 +0200

    update angular

--- webApp/angularapp/src/app/table/table.component.ts
+++ webApp/angularapp/src/app/table/table.component.ts
@@ -7,30 +7,40 @@ import { BooksService } from '../shared/services/books.service';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
-export class TableComponent implements OnInit{
-  allBooks: Book[] = []
-  displayedColumns: string[] = ['author', 'aaName', 'series', 'ownedOrWish', 'status', 'score', 'comment', 'tropes', 'genres']
-  constructor(private booksService: BooksService, public dialog: MatDialog) {
-
-  }
+export class TableComponent implements OnInit {
+  allBooks: Book[] = [];
+  displayedColumns: string[] = [
+    'author',
+    'aaName',
+    'series',
+    'ownedOrWish',
+    'status',
+    'score',
+    'comment',
+    'tropes',
+    'genres',
+  ];
+  constructor(private booksService: BooksService, public dialog: MatDialog) {}
   ngOnInit() {
-    this.getBooks()
+    this.getBooks();
   }
 
   getBooks() {
-    this.booksService.getBooks().subscribe(b => {
-      this.allBooks = b
-    })
+    this.booksService.getBooks().subscribe((b) => {
+      this.allBooks = b;
+    });
   }
 
   createBook() {
-    const dialogRef = this.dialog.open(CreateBookComponent)
-      .afterClosed().subscribe(d => {
+    const dialogRef = this.dialog
+      .open(CreateBookComponent)
+      .afterClosed()
+      .subscribe((d) => {
         if (d) {
-          this.getBooks()
+          this.getBooks();
         }
-      })
+      });
   }
 }
