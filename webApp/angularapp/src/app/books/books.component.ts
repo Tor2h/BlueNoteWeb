25e9c2f371e1bc5788e3b721e8f3a4db0c6363a3
Tor2H <tor.holm@live.dk>
2025-01-14 22:29:20 +0100

    setup

--- webApp/angularapp/src/app/books/books.component.ts
+++ webApp/angularapp/src/app/books/books.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
-import { Book } from '../shared/models/Book';
-import { BooksService } from '../shared/services/books.service';
+import { Component } from "@angular/core";
+import { Book } from "../shared/models/Book";
+import { BooksService } from "../shared/services/books.service";
 
 @Component({
-  selector: 'app-books',
-  templateUrl: './books.component.html',
-  styleUrl: './books.component.css',
+  selector: "app-books",
+  templateUrl: "./books.component.html",
+  styleUrl: "./books.component.css",
 })
 export class BooksComponent {
+  totalBooks = 0;
   books: Book[] = [];
   constructor(private booksService: BooksService) {
     this.booksService.getBooks().subscribe((b) => {
       this.books = b;
-      console.log(b);
+      this.totalBooks = b.length;
     });
   }
 }
