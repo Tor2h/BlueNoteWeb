fa029e583b6d3f0cda2094b95b1b461e68ce8cee
Tor2H <tor.holm@live.dk>
2025-01-23 15:49:56 +0100

    correction

--- webApp/angularapp/src/app/shared/services/books.service.ts
+++ webApp/angularapp/src/app/shared/services/books.service.ts
@@ -1,25 +1,25 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Book } from "../models/Book";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Book } from '../models/Book';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class BooksService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   public getBooks(): Observable<Book[]> {
-    const books = this.http.get<Book[]>("/books");
+    const books = this.http.get<Book[]>('/books');
     return books;
   }
 
   public createBook(book: Book): Observable<boolean> {
     console.log(book);
-    return this.http.post<boolean>("/books", book, {});
+    return this.http.post<boolean>('/books', book, {});
   }
 
   public updateBook(book: Book): Observable<Book> {
-    return this.http.patch<Book>("/books", book, {});
+    return this.http.patch<Book>('/books', book, {});
   }
 }
--- webApp/webapi/BLL/BookManager.cs
+++ webApp/webapi/BLL/BookManager.cs
@@ -4,120 +4,120 @@ using webapi.Models;
 
 namespace webapi.BLL
 {
-    public class BookManager
+  public class BookManager
+  {
+    private readonly IConfiguration _configuration;
+    private readonly IBookDB _bookDB;
+
+    public BookManager(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
-        private readonly IBookDB _bookDB;
-
-        public BookManager(IConfiguration configuration)
-        {
-            this._configuration = configuration;
-            this._bookDB = new BookDB(_configuration);
-        }
-
-        public async Task<List<BookDTO>> GetBooks()
-        {
-            List<Book> books = await _bookDB.GetBooks();
-
-            List<BookDTO> bookDTOs = new List<BookDTO>();
-            foreach (Book book in books)
-            {
-                BookDTO bookDTO = new BookDTO
-                {
-                    AaName = book.AaName,
-                    Author = book.Author,
-                    Series = book.Series,
-                    OwnedOrWish = book.OwnedOrWish,
-                    Status = book.Status,
-                    Score = book.Score,
-                    Comment = book.Comment,
-                };
-                foreach (BookTrope bt in book.BookTropes)
-                {
-                    if (bt.Trope != null)
-                    {
-                        TropeDTO trope = new TropeDTO
-                        {
-                            ID = bt.TropeID.ToString(),
-                            Name = bt.Trope.Name,
-                        };
-                        bookDTO.Tropes.Add(trope);
-                    }
-                }
-                foreach (BookGenre bg in book.BookGenres)
-                {
-                    if (bg.Genre != null)
-                    {
-                        GenreDTO genre = new GenreDTO
-                        {
-                            ID = bg.GenreID.ToString(),
-                            Name = bg.Genre.Name,
-                        };
-                        bookDTO.Genres.Add(genre);
-                    }
-                }
-                bookDTOs.Add(bookDTO);
-            }
-            return bookDTOs;
-        }
-
-        public async Task<bool> CreateBook(BookDTO bookDTO)
-        {
-            Book book = new Book
-            {
-                ID = Guid.NewGuid(),
-                AaName = bookDTO.AaName,
-                Author = bookDTO.Author,
-                Series = bookDTO.Series,
-                Comment = bookDTO.Comment,
-            };
-
-            if (bookDTO.OwnedOrWish != null)
-            {
-                book.OwnedOrWish = (bool)bookDTO.OwnedOrWish;
-            }
-            if (bookDTO.Status != null)
-            {
-                book.Status = (Status)bookDTO.Status;
-            }
-            if (bookDTO.Score != null)
-            {
-                book.Score = (Score)bookDTO.Score;
-            }
-            if (bookDTO.Genres.Count > 0)
-            {
-                foreach (GenreDTO genreDTO in bookDTO.Genres)
-                {
-                    Genre genre = new Genre { ID = Guid.Parse(genreDTO.ID), Name = genreDTO.Name };
-                    BookGenre bg = new BookGenre
-                    {
-                        Book = book,
-                        BookID = book.ID,
-                        ID = Guid.NewGuid(),
-                        Genre = genre,
-                        GenreID = genre.ID,
-                    };
-                    book.BookGenres.Add(bg);
-                }
-            }
-            if (bookDTO.Tropes.Count > 0)
-            {
-                foreach (TropeDTO tropeDTO in bookDTO.Tropes)
-                {
-                    Trope trope = new Trope { ID = Guid.Parse(tropeDTO.ID), Name = tropeDTO.Name };
-                    BookTrope bt = new BookTrope
-                    {
-                        Book = book,
-                        BookID = book.ID,
-                        ID = Guid.NewGuid(),
-                        Trope = trope,
-                        TropeID = trope.ID,
-                    };
-                    book.BookTropes.Add(bt);
-                }
-            }
-
-            return await _bookDB.CreateBook(book);
-        }
+      this._configuration = configuration;
+      this._bookDB = new BookDB(_configuration);
     }
+
+    public async Task<List<BookDTO>> GetBooks()
+    {
+      List<Book> books = await _bookDB.GetBooks();
+
+      List<BookDTO> bookDTOs = new List<BookDTO>();
+      foreach (Book book in books)
+      {
+        BookDTO bookDTO = new BookDTO
+        {
+          AaName = book.AaName,
+          Author = book.Author,
+          Series = book.Series,
+          OwnedOrWish = book.OwnedOrWish,
+          Status = book.Status,
+          Score = book.Score,
+          Comment = book.Comment,
+        };
+        foreach (BookTrope bt in book.BookTropes)
+        {
+          if (bt.Trope != null)
+          {
+            TropeDTO trope = new TropeDTO
+            {
+              ID = bt.TropeID.ToString(),
+              Name = bt.Trope.Name,
+            };
+            bookDTO.Tropes.Add(trope);
+          }
+        }
+        foreach (BookGenre bg in book.BookGenres)
+        {
+          if (bg.Genre != null)
+          {
+            GenreDTO genre = new GenreDTO
+            {
+              ID = bg.GenreID.ToString(),
+              Name = bg.Genre.Name,
+            };
+            bookDTO.Genres.Add(genre);
+          }
+        }
+        bookDTOs.Add(bookDTO);
+      }
+      return bookDTOs;
+    }
+
+    public async Task<bool> CreateBook(BookDTO bookDTO)
+    {
+      Book book = new Book
+      {
+        ID = Guid.NewGuid(),
+        AaName = bookDTO.AaName,
+        Author = bookDTO.Author,
+        Series = bookDTO.Series,
+        Comment = bookDTO.Comment,
+      };
+
+      if (bookDTO.OwnedOrWish != null)
+      {
+        book.OwnedOrWish = (bool)bookDTO.OwnedOrWish;
+      }
+      if (bookDTO.Status != null)
+      {
+        book.Status = (Status)bookDTO.Status;
+      }
+      if (bookDTO.Score != null)
+      {
+        book.Score = (Score)bookDTO.Score;
+      }
+      if (bookDTO.Genres.Count > 0)
+      {
+        foreach (GenreDTO genreDTO in bookDTO.Genres)
+        {
+          Genre genre = new Genre { ID = Guid.Parse(genreDTO.ID), Name = genreDTO.Name };
+          BookGenre bg = new BookGenre
+          {
+            Book = book,
+            BookID = book.ID,
+            ID = Guid.NewGuid(),
+            Genre = genre,
+            GenreID = genre.ID,
+          };
+          book.BookGenres.Add(bg);
+        }
+      }
+      if (bookDTO.Tropes.Count > 0)
+      {
+        foreach (TropeDTO tropeDTO in bookDTO.Tropes)
+        {
+          Trope trope = new Trope { ID = Guid.Parse(tropeDTO.ID), Name = tropeDTO.Name };
+          BookTrope bt = new BookTrope
+          {
+            Book = book,
+            BookID = book.ID,
+            ID = Guid.NewGuid(),
+            Trope = trope,
+            TropeID = trope.ID,
+          };
+          book.BookTropes.Add(bt);
+        }
+      }
+
+      return await _bookDB.CreateBook(book);
+    }
+  }
 }
